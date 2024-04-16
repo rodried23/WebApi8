dd6543731d18ccb4b6f0eb2d8c20623bf066418e
Douglas <rodrieduardo28@outlook.com>
2024-04-15 21:51:24 -0300

    Adicionar arquivos de projeto.

---
+++ WebApi8/Migrations/20240416004509_RemoveColunaTabelaAutor.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace WebApi8.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemoveColunaTabelaAutor : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "MyProperty",
+                table: "Autores");
+        }
+    }
+}
---
+++ WebApi8/Models/LivrosModel.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApi8.Models
+{
+    public class LivrosModel
+    {
+        public int Id { get; set; }
+        public string Titulo { get; set; }
+        public AutorModel Autor { get; set; }
+    }
+}
