338493e67a41269c4ac6d9975b43abc5adce05c6
Lucas Andrade dos Santos <lucas.santos@tbfg.com.br>
2024-04-22 17:17:36 -0300

    iniciando implementação Service Pessoa

---
+++ WebApi8/Services/Pessoa/IPessoaInterface.cs
@@ -0,0 +1,16 @@
+﻿using WebApi8.Dto.Pessoa;
+using WebApi8.Models;
+
+
+namespace WebApi8.Services.Pessoa
+{
+    public interface IPessoaInterface
+    {
+        Task<ResponseModel<List<PessoaModel>>> CadastrarPessoa(PessoaCriacaoDto pessoaCriacaoDto);
+        Task<ResponseModel<List<PessoaModel>>> EditarPessoa(PessoaEdicaoDto pessoaEdicaoDto);
+        Task<ResponseModel<List<PessoaModel>>> ExcluirCadastroPessoa(int idPessoa);
+        Task<ResponseModel<List<PessoaModel>>> ListarPessoas();
+        Task<ResponseModel<PessoaModel>> BuscarPessoaPorId(int idPessoa);
+        Task<ResponseModel<List<PessoaModel>>> BuscarPessoaPorCpf(string cpfPessoa);
+    }
+}
---
+++ WebApi8/Services/Pessoa/PessoaService.cs
@@ -0,0 +1,38 @@
+﻿using WebApi8.Dto.Pessoa;
+using WebApi8.Models;
+
+namespace WebApi8.Services.Pessoa
+{
+    public class PessoaService : IPessoaInterface
+    {
+        public Task<ResponseModel<List<PessoaModel>>> BuscarPessoaPorCpf(string cpfPessoa)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResponseModel<PessoaModel>> BuscarPessoaPorId(int idPessoa)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResponseModel<List<PessoaModel>>> CadastrarPessoa(PessoaCriacaoDto pessoaCriacaoDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResponseModel<List<PessoaModel>>> EditarPessoa(PessoaEdicaoDto pessoaEdicaoDto)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResponseModel<List<PessoaModel>>> ExcluirCadastroPessoa(int idPessoa)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ResponseModel<List<PessoaModel>>> ListarPessoas()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
